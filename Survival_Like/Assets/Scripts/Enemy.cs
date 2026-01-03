b6441fb1df2f39e6577d6c9a1e502a33b235d16f
hero <hanwoong2042@naver.com>
2026-01-03 23:38:21 +0900

    몬스터 생성

---
+++ Survival_Like/Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D target;
+
+    bool isLive = true;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isLive)
+            return;
+
+        Vector2 dirVec = target.position - rigid.position;
+        Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    private void LateUpdate()
+    {
+        if (!isLive)
+            return;
+
+        spriteRenderer.flipX = target.position.x < rigid.position.x;
+    }
+
+
+}
--- Survival_Like/Assets/Scripts/Reposition.cs
+++ Survival_Like/Assets/Scripts/Reposition.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
+    Collider2D coll;
+
+    private void Awake()
+    {
+        coll = GetComponent<Collider2D>();
+    }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (!collision.CompareTag("Area"))
@@ -33,6 +40,10 @@ public class Reposition : MonoBehaviour
                     break;
 
             case "Enemy":
+                if(coll.enabled)
+                {
+                    transform.Translate(playerDir * 25 + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0f));
+                }
                 break;
         }
     }
